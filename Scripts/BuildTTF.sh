0e05a99345ab937333f65c84557a1a3bed9e68d1
Shiyu He <shiyuhe@laioffer.com>
2019-01-09 12:27:18 -0800

    Update macOS & Linux building scripts.

--- Scripts/BuildTTF.sh
+++ Scripts/BuildTTF.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # This bash script generates TrueType version of Source Han Sans Subset.
-# Note that this script depends on Adobe's AFDKO and otrebuilder, both on GitHub.
+# Note that this script depends on Adobe's AFDKO and otRebuilder, both of which could be found on GitHub.
 
-# Convert otf files into ttf files with UPM 2048.
+# Clean-up of previous output files.
+rm -rf Subset
+
+# Create output folder.
+mkdir Subset
+
+# Locate to the work directory.
+cd source-han-sans-2.000R/SubsetOTF
+
+# Convert otf files into ttf files in UPM 2048.
 for folder in CN TW HK JP KR
 do
 	cd $folder
@@ -10,8 +19,8 @@ do
 	do
 		fontFileName="${fontPath##*/}"
 		fontFilePre="${fontFileName%.*}"
-		../otrebuild --otf2ttf --UPM 2048 --removeGlyphNames --O1 -o $fontFilePre.ttf $fontPath
-		rm $fontPath
+		../../../otrebuild-mac --otf2ttf --UPM 2048 --removeGlyphNames --O1 -o $fontFilePre.ttf $fontPath
+		mv -f $fontFilePre.ttf ../../../Subset/
 	done
 	cd ..
 done
