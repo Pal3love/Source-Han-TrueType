fb2043421b1abc71320feedf2b6059615e767ac6
Pal3love <Pal3love@users.noreply.github.com>
2022-03-26 01:12:25 -0700

    Refactor building script and reorganize the repo. (#13)

--- README.md
+++ README.md
@@ -1,42 +1,85 @@
-# Source Han TrueType | 思源 TrueType
-**TrueType Version of Source Han Sans &amp; Source Han Serif | 思源黑体和思源宋体的 TrueType 版本**
+[English](README-EN.md)
 
-The Adobe-branded *Source Han Sans*, *Source Han Serif*, and *Source Han Mono* are open source Pan-CJK typefaces whose OpenType/CFF fonts and CID-based sources are covered under the terms of the [SIL Open Font License, Version 1.1](http://scripts.sil.org/OFL) (also see the [FAQ](http://scripts.sil.org/cms/scripts/page.php?item_id=OFL-FAQ_web)).
+# 思源 TrueType
+**思源黑体、思源宋体和思源等宽的 TrueType 版本**
 
 **思源黑体**、**思源宋体**以及**思源等宽**是由 Adobe 主导开发的开源及跨中日韩地区的多语言字库。这些字库的 OpenType/CFF 文件以及 CID 源文件均由 [SIL 开源字库协议，版本 1.1](http://scripts.sil.org/OFL) 授权（另请参阅[常见问题](http://scripts.sil.org/cms/scripts/page.php?item_id=OFL-FAQ_web)）。
 
-This project includes the *TrueType version* of all the fonts above, which generally presents better compatibility on Microsoft Windows, Microsoft Office and front-end-specific fields than OpenType/CFF version.
-
-本项目提供以上所有字库的 **TrueType 版本**。通常 TrueType 格式的字库在 Microsoft Windows、Microsoft Office 以及前端领域的兼容性要优于 OpenType/CFF 格式。
+本项目在完整保留曲线精度、尺寸和功能的前提下，提供思源所有字体的 **TrueType 版本**。TrueType 格式在 Microsoft Windows、Microsoft Office 以及前端领域的兼容性要优于 OpenType/CFF (OTF) 格式。本项目提供的所有 TrueType 字体均可与思源 OTF 原版无缝替换，无需返工修订设计文稿。
 
 
-## Specification | 技术规格
+## 下载与安装
 
-* Language-specific version: TrueType collection (TTC)
-* 多语言完整版（65,535 字符）：TrueType collection (TTC)
-* Region-specific subset version: TrueType (TTF)
-* 多地区子集版：TrueType (TTF)
-* Units-per-em (UPM): 2048, i.e. *lossless conversion from the OpenType/CFF source*
-* 曲线精度：2048，**相当于 OpenType/CFF 原版的无损转换**
-* Rendering strategy: full-range subpixel anti-aliasing (on Windows 10)
-* 渲染策略：全字号亚像素抗锯齿（Windows 10）
-* OpenType features: All included
-* OpenType 特性：完整收录
-
-
-## Font Installation Instructions | 字库安装指南
+请前往[**发布页面**](https://github.com/Pal3love/Source-Han-TrueType/releases)下载最新版本的压缩包，解压后即可得到 TTF/TTC 字体文件。当前页面的“Code”按钮仅包含转换所需的代码和源文件，不包含 TrueType 字体。
 
 * [macOS](https://support.apple.com/en-us/HT201749)
+* [Linux](https://github.com/adobe-fonts/source-code-pro/issues/17#issuecomment-8967116)
 * [Windows](https://www.microsoft.com/en-us/Typography/TrueTypeInstall.aspx)
-* [Linux/Unix-based systems](https://github.com/adobe-fonts/source-code-pro/issues/17#issuecomment-8967116)
+* **Windows 用户请注意**：从 Windows 10 1809 开始，Windows 会将字体文件默认安装到用户文件夹下，该行为可能会导致一部分软件找不到字体。建议在字体文件上单击右键，选择“为所有用户安装”，以全局安装。
 
 
-## Further Information | 更多信息
+## 技术规格
 
-For information about the design and background of the Source Han typefaces, please refer to their respective repositories.
+* **OpenType 功能（竖排支持等）**：完整收录
+* **Microsoft Office Style-Link**：加粗按钮（B）链接常规与粗体字重
+* **Microsoft Office 字体嵌入**：支持 Word、Excel、PowerPoint 等软件的字体嵌入功能
+* **封装格式**：完整版为 TrueType Collection (TTC)，子集版为 TrueType (TTF)
+* **曲线格式**：二次贝塞尔曲线
+* **曲线精度（UPM）**：2048，即原版 OpenType/CFF 三次曲线的无损转换
+* **屏显渲染策略**：全字号亚像素抗锯齿（Windows 10）
 
-关于字库设计以及其他有关思源字库的信息，请垂询各个 GitHub 仓库。
 
-* Source Han Sans: https://github.com/adobe-fonts/source-han-sans
-* Source Han Serif: https://github.com/adobe-fonts/source-han-serif
-* Source Han Mono: https://github.com/adobe-fonts/source-han-mono
+## 可变版本
+
+Adobe 官方已提供思源黑体和思源宋体的可变版 TrueType 字体文件，请前往以下发布页面下载。
+
+* [思源黑体可变版](https://github.com/adobe-fonts/source-han-sans/releases)
+* [思源宋体可变版](https://github.com/adobe-fonts/source-han-serif/releases)
+
+
+## Adobe 软件的行高过大问题
+
+Adobe 软件（如 Photoshop、Illustrator 等）的行高过大问题是由 OTF 原版字体中的“3 em vertical dash”字符所导致的。思源字体的全局字框为了容纳该字符而撑得很高。本项目与思源 OTF 原版的所有功能保持完全一致，因而**不会**修改该设计。**如需修正行高的版本，请前往[梦源字体系列](https://github.com/Pal3love/dream-han-cjk)**。该系列除修正行高之外，更提供了多达 29 个字重的丰富选择。
+
+
+## 编译
+
+如需在本地完成转换过程，请参考以下指南。
+
+### 硬盘空间
+
+脚本将生成约 5 GB 临时文件，加上最终的 ZIP 压缩包共计约 6 GB 总硬盘空间。临时文件将在运行结束时自动删除。
+
+### 平台依赖
+
+本项目支持 Windows Linux 子系统（WSL）与 macOS 平台。请先确保以下依赖已安装：
+
+* Python 3.8 及以上
+* PyPI 包管理（`pip`）
+* `pip install afdko`: [Adobe Font Development Kit for OpenType (AFDKO)](https://github.com/adobe-type-tools/afdko)
+* `sudo apt install zip`（仅供 WSL，macOS 已自带）
+* `sudo apt install rename`（仅供 WSL，macOS 已自带）
+
+### OTF 源文件
+
+为节约体积，本仓库不自带原版思源 OTF 字体文件。请前往官方仓库下载 OTF 字体并分别放在 source 目录内的三个子目录内：
+
+* 请将[思源等宽的所有 OTF 文件](https://github.com/adobe-fonts/source-han-mono/archive/refs/heads/master.zip)放在 source-han-mono 目录内；
+* 请将[思源黑体的所有 OTF 文件](https://github.com/adobe-fonts/source-han-sans/releases)放在 source-han-sans 目录内；
+* 请将[思源宋体的所有 OTF 文件](https://github.com/adobe-fonts/source-han-serif/releases)放在 source-han-serif 目录内。
+
+### 执行脚本
+
+* WSL: `cd` 进 script 目录后，执行 `./build_ttfs.sh wsl <最大并行数>`
+* macOS: `cd` 进 script 目录后，执行 `./build_ttfs.sh mac <最大并行数>`
+* 其中，**最大并行数**决定最多并行处理的字体数量。每个字体占用约 1.5 CPU 线程和最多 600 MB 内存，请根据自己的电脑配置酌情选择。譬如，AMD Ryzen 9 3950X 可将此参数设为 28，在内存足够的情况下，可达 97% CPU 使用率。
+* 脚本运行完成后，最终字体的 ZIP 压缩包将位于根目录下新创建的 release 目录内。运行过程中产生的临时文件将会在结束时自动删除。
+
+
+## 更多信息
+
+如需获取 Adobe 思源系列字体的设计、使用以及其他信息，请访问以下官方 GitHub 仓库：
+
+* [思源黑体](https://github.com/adobe-fonts/source-han-sans)
+* [思源宋体](https://github.com/adobe-fonts/source-han-serif)
+* [思源等宽](https://github.com/adobe-fonts/source-han-mono)
---
+++ script/build_ttfs.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+
+maximum_parallels=1
+current_parallels=0
+
+script_file_name="build_ttfs.sh"
+otrebuild_win="otrebuild_win.exe"
+otrebuild_mac="otrebuild_mac"
+otrebuild_binary=${otrebuild_win}
+
+binary_folder="binary"
+release_folder="release"
+script_folder="script"
+source_folder="source"
+temp_folder="temp"
+
+font_grand_family="SourceHan"
+font_mono_typeface="Mono"
+font_sans_typeface="Sans"
+font_sans_hw_typeface="SansHW"
+font_serif_typeface="Serif"
+font_lingual_typefaces=(${font_sans_hw_typeface} ${font_mono_typeface})
+font_regional_typefaces=(${font_sans_typeface} ${font_serif_typeface})
+font_typefaces=(${font_lingual_typefaces[@]} ${font_regional_typefaces[@]})
+
+font_languages=(SC TC HC J K)
+font_regions=(CN TW HK JP KR)
+font_languages_and_regions=(${font_languages[@]} ${font_regions[@]})
+
+font_mono_italic_appendix="It"
+font_shared_weights=(ExtraLight Light Medium Heavy)
+font_hw_weights=(Regular Bold)
+font_sans_mono_special_weights=(Normal)
+font_serif_spectial_weights=(SemiBold)
+font_sans_mono_weights=(${font_shared_weights[@]} ${font_hw_weights[@]} ${font_sans_mono_special_weights[@]})
+font_serif_weights=(${font_shared_weights[@]} ${font_hw_weights[@]} ${font_serif_spectial_weights[@]})
+
+
+function print_usage_and_exit() {
+    echo "Usage: ${script_file_name} <platform: wsl or mac> <maximum parallel jobs>"
+    exit 1
+}
+
+function is_number() {
+    [[ $1 =~ ^[0-9]+$ ]]
+}
+
+function set_platform() {
+    if [ $1 == "mac" ]; then
+        otrebuild_binary=${otrebuild_mac}
+    elif [ $1 != "wsl" ]; then
+        print_usage_and_exit
+    fi
+    echo "Set platform to $1"
+}
+
+function set_parallels() {
+    if ! is_number $1; then
+        print_usage_and_exit
+    elif [ $1 -gt 1 ]; then
+        maximum_parallels=$1
+    fi
+    echo "Set maximum parallel jobs to ${maximum_parallels}"
+}
+
+function limit_parallels() {
+    current_parallels=`expr ${current_parallels} + 1`
+    if [ ${current_parallels} -gt ${maximum_parallels} ]; then
+        wait
+        current_parallels=1
+    fi
+}
+
+function convert() {
+    for font_path in *.otf; do
+        font_file_name="${font_path##*/}"
+        font_file="${font_file_name%.*}"
+        limit_parallels; {
+            ../../${binary_folder}/${otrebuild_binary} --otf2ttf --UPM 2048 --removeGlyphNames --O1 -o ../../${temp_folder}/${font_file}.ttf ${font_path}
+        } &
+    done
+    wait
+}
+
+function add_japanese_language_code() {
+    for font_typeface in ${font_typefaces[@]}; do
+        rename "s/${font_grand_family}${font_typeface}-/${font_grand_family}${font_typeface}J-/" ${font_grand_family}${font_typeface}-*.ttf
+    done
+}
+
+function make_ttc() {
+    local font_typeface=$1  # Save first argument in a variable
+    shift  # Shift all arguments to the left (original $1 gets lost)
+    local font_weights=("$@")  # Rebuild the array with rest of arguments
+    font_family="${font_grand_family}${font_typeface}"
+    for font_weight in ${font_weights[@]}; do
+        ttfs=""
+        for font_language in ${font_languages[@]}; do
+            ttfs+="${font_family}${font_language}-${font_weight}.ttf "
+        done
+        otf2otc -o ${font_family}-${font_weight}.ttc ${ttfs}
+    done
+}
+
+function make_ttcs() {
+    font_mono_italics=()
+    for font_sans_mono_weight in ${font_sans_mono_weights[@]}; do
+        font_mono_italics+=("${font_sans_mono_weight}${font_mono_italic_appendix}")
+    done
+    make_ttc ${font_mono_typeface} ${font_mono_italics[@]}
+    make_ttc ${font_mono_typeface} ${font_sans_mono_weights[@]}
+    make_ttc ${font_sans_typeface} ${font_sans_mono_weights[@]}
+    make_ttc ${font_sans_hw_typeface} ${font_hw_weights[@]}
+    make_ttc ${font_serif_typeface} ${font_serif_weights[@]}
+}
+
+function compress_ttc() {
+    for font_typeface in ${font_typefaces[@]}; do
+        limit_parallels; {
+            ttcs=""
+            for ttc_path in `ls ${font_grand_family}${font_typeface}-*.ttc`; do
+                ttcs+="${ttc_path} "
+            done
+            zip ../${release_folder}/${font_grand_family}${font_typeface}.zip ${ttcs}
+        } &
+    done
+    wait
+}
+
+function compress_ttf() {
+    for font_typeface in ${font_regional_typefaces[@]}; do
+        for font_region in ${font_regions[@]}; do
+            limit_parallels; {
+                ttfs=""
+                for ttf_path in `ls ${font_grand_family}${font_typeface}${font_region}-*.ttf`; do
+                    ttfs+="${ttf_path} "
+                done
+                zip ../${release_folder}/${font_grand_family}${font_typeface}${font_region}.zip ${ttfs}
+            } &
+        done
+    done
+    wait
+}
+
+function main() {
+    # Go to root.
+    cd ..
+
+    # Clean up & recreate temp & output folders.
+    rm -rf ${temp_folder}
+    rm -rf ${release_folder}
+    mkdir ${temp_folder}
+    mkdir ${release_folder}
+
+    # Convert all OTFs into TTFs.
+    cd ${source_folder}/source-han-mono
+    convert
+    cd ../source-han-sans
+    convert
+    cd ../source-han-serif
+    convert
+    cd ../..
+
+    # Insert Japanese language code "J".
+    cd ${temp_folder}
+    add_japanese_language_code
+
+    # Combine lingual fonts into TTC files.
+    make_ttcs
+
+    # Compress font files into ZIPs.
+    compress_ttc
+    compress_ttf
+
+    # Clean up.
+    rm -f *.ttf
+    rm -f *.ttc
+    cd ..
+    rm -rf ${temp_folder}
+}
+
+
+if [ $# != 2 ]; then
+    print_usage_and_exit
+else
+    set_platform $1
+    set_parallels $2
+    sleep 3
+    main
+fi
